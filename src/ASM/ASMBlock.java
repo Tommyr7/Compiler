e48a37176b99afc35a42d61c04ae7a7b00685cdc
Tommyr7 <3265612679@qq.com>
2021-07-20 03:01:56 +0800

    -codegen

--- src/ASM/ASMBlock.java
+++ src/ASM/ASMBlock.java
@@ -18,7 +18,7 @@ public class ASMBlock {
         prt.println("\t.globl\t" + id);
         prt.println("\t.type\t" + id + ", @function");
         prt.println(id + ":");
-        int size = 4 * (cnt + 1);
+        int size = 4 * (cnt + 2);
         prt.println("\taddi\tsp,sp,-" + String.valueOf(size));
         prt.println("\tsw\tra,0(sp)");
         inst.forEach(x -> x.print(prt));
